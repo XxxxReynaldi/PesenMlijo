f3b98ebb7fbd77b3b6675a6c8f5a70e8dc5c6a1b
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2021-04-08 12:36:17 +0700

    task 02 - preview produk
    belum terkoneksi database

---
+++ app/src/main/java/com/semutunic/pesenmlijo/activities/ProdukAdapter.java
@@ -0,0 +1,77 @@
+package com.semutunic.pesenmlijo.activities;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.semutunic.pesenmlijo.R;
+
+import java.util.List;
+
+public class ProdukAdapter extends RecyclerView.Adapter<ProdukAdapter.ProductViewHolder> {
+
+
+    //this context we will use to inflate the layout
+    private Context mCtx;
+
+    //we are storing all the products in a list
+    private List<ProdukModel> productList;
+
+    //getting the context and product list with constructor
+    public ProdukAdapter(Context mCtx, List<ProdukModel> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.item_add_produk, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        //getting the product of the specified position
+        ProdukModel product = productList.get(position);
+
+        //binding the data with the viewholder views
+        holder.namaproduk.setText(product.getNamaProduk());
+//        holder.textViewShortDesc.setText(product.getShortdesc());
+//        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.hargaproduk.setText(String.valueOf(product.getHargaProduk()));
+
+        holder.image_produk.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView namaproduk, hargaproduk;
+        ImageView image_produk;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+            namaproduk = itemView.findViewById(R.id.namaP);
+//            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+//            textViewRating = itemView.findViewById(R.id.textViewRating);
+            hargaproduk = itemView.findViewById(R.id.hargaP);
+            image_produk = itemView.findViewById(R.id.image_produk);
+        }
+    }
+}
