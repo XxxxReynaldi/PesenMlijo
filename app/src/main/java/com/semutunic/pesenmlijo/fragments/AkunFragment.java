8e3fffc3a6956337aec1a2847f9a570eae31e116
Zulfi Rizkiawan <zulfi.rizkiawan.27@gmail.com>
2021-04-26 07:40:09 +0700

    F AKUN, PESANAN

--- app/src/main/java/com/semutunic/pesenmlijo/fragments/AkunFragment.java
+++ app/src/main/java/com/semutunic/pesenmlijo/fragments/AkunFragment.java
@@ -7,32 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.semutunic.pesenmlijo.R;
 
-import java.util.ArrayList;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PesananFragment#newInstance} factory method to
+ * Use the {@link AkunFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PesananFragment extends Fragment {
+public class AkunFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public PesananFragment() {
-
+    public AkunFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +36,11 @@ public class PesananFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PesananFragment.
+     * @return A new instance of fragment AkunFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PesananFragment newInstance(String param1, String param2) {
-        PesananFragment fragment = new PesananFragment();
+    public static AkunFragment newInstance(String param1, String param2) {
+        AkunFragment fragment = new AkunFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -57,7 +51,6 @@ public class PesananFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -68,6 +61,6 @@ public class PesananFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pesanan, container, false);
+        return inflater.inflate(R.layout.fragment_akun, container, false);
     }
 }
